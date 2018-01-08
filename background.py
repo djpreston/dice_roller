133b397582dcb59730af4c3576260994c9160119
djpreston <djpreston@crimson.ua.edu>
2018-01-08 10:22:25 -0500

    added img_proc.py

---
+++ background.py
@@ -0,0 +1,18 @@
+import time
+import cv2
+
+def take_background():
+    cam = cv2.VideoCapture(0)
+    ret_val, img = cam.read()
+    cv2.imshow('my webcam', img)
+#    time.sleep(5)
+    cv2.waitKey(1)
+    cv2.imwrite("background.png", img)
+
+def main():
+    take_background()
+
+if __name__ == '__main__':
+    main()
+
+
