7b4a04d304a825376a57b20dbde14d5129c42f58
djpreston <djpreston@crimson.ua.edu>
2018-02-27 21:39:58 -0500

    redcolor

--- img_proc_fcn.py
+++ img_proc_fcn.py
@@ -10,18 +10,33 @@ def take_image():
 #    cv2.waitKey(1)
 #    cv2.imwrite("background.png", img)
 
-def cap_and_proc():
+def cap_and_proc(main_i):
     background = cv2.imread("background.png",1)
 #    cv2.imshow('subtracted image', background)
 #    print background.shape
 #    cv2.waitKey(0)
     background = cv2.cvtColor(background, cv2.COLOR_BGR2GRAY)
     img = take_image()
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    img_no_bg = cv2.subtract(img, background)
+    img_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    img_bw = img_hsv[:,:,0]
+    img_bw_sat = img_hsv[:,:,1]
+    _,img_bw_hi = cv2.threshold(img_bw,240,255,cv2.THRESH_BINARY)
+    _,img_bw_lo = cv2.threshold(img_bw,40,255,cv2.THRESH_BINARY)
+    img_bw_lo = cv2.bitwise_not(img_bw_lo)
+    img_bw = cv2.bitwise_or(img_bw_hi, img_bw_lo)
+    _,img_bw_lo_sat = cv2.threshold(img_bw_sat,20,255,cv2.THRESH_BINARY)
+#    img_bw_lo_sat = cv2.bitwise_not(img_bw_lo_sat)
+    img_bw2 = cv2.bitwise_and(img_bw, img_bw_lo_sat)
+    img_bw = cv2.bitwise_and(img_bw, img_bw_lo_sat)
+    img_bin = img_bw
+#    img_bin = cv2.bitwise_not(img_bw)
+#    img_bw = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+#    img_no_bg = cv2.subtract(img_bw, background)
 #    img_no_bg_inv = cv2.subtract(background, img)
-    _,img_bin = cv2.threshold(img_no_bg,30,255,cv2.THRESH_BINARY)
-#    _,img_bin_inv = cv2.threshold(img_no_bg_inv,100,255,cv2.THRESH_BINARY)
+#replaced    _,img_bin = cv2.threshold(img_no_bg,10,255,cv2.THRESH_BINARY)
+#    _,img_bin = cv2.threshold(img,215,255,cv2.THRESH_BINARY)
+#    _,img_bin_inv = cv2.threshold(img_no_bg_inv,10,255,cv2.THRESH_BINARY)
+#    img_bin = img_bin_inv
 #    img_bin_inv = cv2.bitwise_not(img_bin_inv)
 #    img_bin = cv2.bitwise_or(img_bin, img_bin_inv)
 #    img_thresh = img_bin
@@ -29,10 +44,13 @@ def cap_and_proc():
 #    cv2.waitKey()
 #    img_bin = cv2.floodFill(img_bin, (0,0), 1, (0,)*3, (0,)*3, flags=4|cv2.FLOODFILL_MASK_ONLY, mask=None)
     cv2.floodFill(img_bin, None, (0,0), 255)
+    cv2.floodFill(img_bin, None, (630,0), 255)
+    cv2.floodFill(img_bin, None, (630,450), 255)
+    cv2.floodFill(img_bin, None, (0,450), 255)
 #    print img_bin
     params = cv2.SimpleBlobDetector_Params()
     params.filterByInertia = True
-    params.minInertiaRatio = 0.5
+    params.minInertiaRatio = 0.3
 
     cv_ver = (cv2.__version__).split('.')
     if int(cv_ver[0]) < 3 :
@@ -47,12 +65,19 @@ def cap_and_proc():
 #    print('You rolled %d'%n_dice)
 #    print dir(keypoints)
 #    cv2.imshow('subtracted image', img_bin)
-    cv2.imshow('image', img)
-    cv2.waitKey(0)
+    cv2.putText(img_bin,str(n_dice),(20,70),cv2.FONT_HERSHEY_SIMPLEX, 2, 0)
+    cv2.imwrite("/home/gmw94193/dice_roller/proc_img/raw%d.png"%main_i, img_bin)
+    cv2.imwrite("/home/gmw94193/dice_roller/raw_img/proc%d.png"%main_i, img)
+    cv2.imshow('image', img_bw2)
+    cv2.waitKey(1000)
+    cv2.destroyAllWindows()
     return n_dice
 
 def main():
-    print "main"
+#    print "main"
+    result = cap_and_proc()
+    time.sleep(1)
+    print result
 
 if __name__ == '__main__':
     main()
--- main_loop.py
+++ main_loop.py
@@ -5,12 +5,22 @@ import img_proc_fcn
 from simple_com import arduinoCommunicator
 
 if __name__=="__main__":
+  data = [0,0,0,0,0,0]
+  for i in range(4):
     d = arduinoCommunicator()
-    result = img_proc_fcn.cap_and_proc()
-    print result
 #    d.blink()
-    d.writeCom(255)
-    time.sleep(1)
     d.writeCom(255)
     time.sleep(2)
     d.writeCom(0)
+    time.sleep(0)
+    d.writeCom(0)
+    time.sleep(0)
+    d.writeCom(0)
+    time.sleep(4)
+    result = img_proc_fcn.cap_and_proc(i)
+    print result
+    if ((result >= 1) and (result <= 6)):
+      data[result-1] = data[result-1]+1
+    print data
+  print data
+
